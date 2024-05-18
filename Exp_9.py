0e021c899ccef3e231fb68663823a4b4b12698d6
M O H I T  K U M A R <141761768+mhtkr@users.noreply.github.com>
2024-05-18 20:18:19 +0530

    Create Exp_9.py

---
+++ Exp_9.py
@@ -0,0 +1,39 @@
+import random
+
+words = [
+    'rain', 'king', 'rope', 'speed',
+    'python', 'mathematics', 'player', 'condition',
+    'reverse', 'water', 'board', 'geeks'
+]
+
+word = random.choice(words)
+print("Guess the characters")
+
+guesses = ''
+turns = 12
+
+while turns > 0:
+    failed = 0
+    for char in word:
+        if char in guesses:
+            print(char, end=" ")
+        else:
+            print("_", end=" ")
+            failed += 1
+
+    if failed == 0:
+        print("\nYou Win")
+        print("The word is:", word)
+        break
+
+    print()
+    guess = input("Guess a character: ")
+    guesses += guess
+
+    if guess not in word:
+        turns -= 1
+        print("Wrong")
+        print("You have", turns, 'more guesses')
+
+    if turns == 0:
+        print("You Lose")
